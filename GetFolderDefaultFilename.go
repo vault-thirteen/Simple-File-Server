5a9834d2eaddf8278cad5e59388f4177d5d181d8
Vault Thirteen <54213347+vault-thirteen@users.noreply.github.com>
2023-05-24 19:35:12 +0300

    A new feature: A method to get name of a default file for a folder.

---
+++ GetFolderDefaultFilename.go
@@ -0,0 +1,40 @@
+package sfs
+
+import (
+	"errors"
+	"path/filepath"
+)
+
+// GetFolderDefaultFilename returns name of the first found default file of a
+// folder and a flag showing existence of the default file. Path is a relative
+// path inside the file server's root folder.
+func (sfs *SimpleFileServer) GetFolderDefaultFilename(relPath string) (fileName string, fileExists bool, err error) {
+	if len(sfs.folderDefaultFiles) == 0 {
+		return "", false, nil
+	}
+
+	if !IsPathValid(relPath) {
+		return "", false, errors.New(ErrPathIsNotValid)
+	}
+
+	absFolderPath := filepath.Join(sfs.rootFolderPath, relPath)
+
+	var absFilePath string
+	for _, fdf := range sfs.folderDefaultFiles {
+		absFilePath = filepath.Join(absFolderPath, fdf)
+
+		if sfs.isCachingEnabled {
+			fileExists, err = sfs.getFileExistenceUsingCache(absFilePath)
+		} else {
+			fileExists, err = sfs.getFileExistenceWithoutCache(absFilePath)
+		}
+		if err != nil {
+			return "", false, err
+		}
+		if fileExists {
+			return fdf, true, nil
+		}
+	}
+
+	return "", false, nil
+}

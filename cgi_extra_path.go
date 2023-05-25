25bac9d4d7d2135f987709d94f1698448c4d5106
Vault Thirteen <54213347+vault-thirteen@users.noreply.github.com>
2023-05-25 03:44:39 +0300

    New functions to work with 'Extra Path' feature of the CGI interface.

---
+++ cgi_extra_path.go
@@ -0,0 +1,59 @@
+package sfs
+
+import (
+	"errors"
+	"path/filepath"
+	"strings"
+
+	"github.com/vault-thirteen/auxie/file"
+)
+
+const (
+	ErrCgiExtraPathIsNotFound = "CGI extra path is not found"
+)
+
+const (
+	ForwardSlashString  = string(`/`)
+	BackwardSlashString = string(`\`)
+)
+
+// FindExtraPath tries to find CGI extra path.
+// "Extra Path" is a CGI interface feature that allows to make crazy-looking
+// URLs which are impossible to be parsed, something like the following:
+// http://some.machine/cgi-bin/display.pl/cgi/cgi_doc.txt
+func (sfs *SimpleFileServer) FindExtraPath(relPath string) (path string, extraPath string, err error) {
+	curPath := strings.TrimSuffix(relPath, ForwardSlashString)
+	curPath = strings.TrimSuffix(curPath, BackwardSlashString)
+
+	var fileExists bool
+	var absFilePath string
+
+	for {
+		// Check.
+		absFilePath = filepath.Join(sfs.rootFolderPath, curPath)
+		fileExists, err = sfs.getFileExistenceUsingCache(absFilePath)
+		if (err != nil) && (err.Error() != file.ErrObjectIsNotFile) {
+			return "", "", err
+		}
+		if fileExists {
+			extraPath = strings.TrimPrefix(relPath, curPath)
+			return curPath, extraPath, nil
+		}
+
+		// Next.
+		curPath = shortenPath(curPath)
+		if len(curPath) == 0 {
+			break
+		}
+	}
+
+	return "", "", errors.New(ErrCgiExtraPathIsNotFound)
+}
+
+// shortenPath trims the last segment of the path.
+func shortenPath(path string) (shortenedPath string) {
+	dir, _ := filepath.Split(path)
+	shortenedPath = strings.TrimSuffix(dir, ForwardSlashString)
+	shortenedPath = strings.TrimSuffix(shortenedPath, BackwardSlashString)
+	return shortenedPath
+}
